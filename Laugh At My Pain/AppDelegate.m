4a32efc725fb894c0e273802741e01afd7cd7718
Andre Villanueva <Andre@AndreVillanueva.com>
2017-08-12 17:41:24 -0600

    gitignore now working

--- Laugh At My Pain/AppDelegate.m
+++ Laugh At My Pain/AppDelegate.m
@@ -13,11 +13,11 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    
+    self.printAppInfo;
     sleep(3);
     return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -26,7 +26,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
@@ -43,6 +43,46 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)printAppInfo{
+    NSString * CFBundleShortVersionString = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleShortVersionString"];
+    NSString * CFBundleDevelopmentRegion = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleDevelopmentRegion"];
+    NSString * CFBundleDisplayName = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleDisplayName"];
+    NSString * CFBundleDocumentTypes = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleDocumentTypes"];
+    NSString * CFBundleDeCFBundleExecutablevelopmentRegion = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleDeCFBundleExecutablevelopmentRegion"];
+    NSString * CFBundleIconFile = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleIconFile"];
+    NSString * CFBundleIcons = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleIcons"];
+    NSString * CFBundleIdentifier = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleIdentifier"];
+    NSString * CFBundleLocalizations = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleLocalizations"];
+    NSString * CFBundleName = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleName"];
+    NSString * CFBundlePackageType = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundlePackageType"];
+    NSString * CFBundleSignature = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleSignature"];
+    NSString * CFBundleSpokenName = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleSpokenName"];
+    NSString * CFBundleURLTypes = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleURLTypes"];
+    NSString * CFBundleVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleVersion"];
+    NSString * CFBundleTypeName = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleTypeName"];
+    NSString * LSHandlerRank = [[NSBundle mainBundle] objectForInfoDictionaryKey: @"LSHandlerRank"];
+    
+    
+//    printf(CFBundleShortVersionString);
+//    printf(CFBundleDevelopmentRegion);
+//    printf(CFBundleDisplayName);
+//    printf(CFBundleDocumentTypes);
+//    printf(CFBundleDeCFBundleExecutablevelopmentRegion);
+//    printf(CFBundleIconFile);
+//    printf(CFBundleIcons);
+//    printf(CFBundleIdentifier);
+//    printf(CFBundleLocalizations);
+//    printf(CFBundleName);
+//    printf(CFBundleSignature);
+//    printf(CFBundleSpokenName);
+//    printf(CFBundleURLTypes);
+//    printf(CFBundleVersion);
+//    printf(CFBundleTypeName);
+//    printf(LSHandlerRank);
+//    printf(CFBundlePackageType);
+    
 }
 
 @end
